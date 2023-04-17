369bfb25adbc77b5cd46f93128d2deae3a7b147b
Quevin Bambasi <quevinbambasi@pm.me>
2023-04-17 14:27:37 -0400

    Initial Commit

---
+++ Recipe List App/Recipe_List_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Recipe_List_AppApp.swift
+//  Recipe List App
+//
+//  Created by Quevin Bambasi on 4/17/23.
+//
+
+import SwiftUI
+
+@main
+struct Recipe_List_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

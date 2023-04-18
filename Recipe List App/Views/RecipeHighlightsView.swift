e430312300b83a419ae14e6e75f542062c685098
Quevin Bambasi <quevinbambasi@pm.me>
2023-04-18 15:31:14 -0400

    Add RecipeHighlightsView functionality

--- Recipe List App/Views/RecipeHighlightsView.swift
+++ Recipe List App/Views/RecipeHighlightsView.swift
@@ -8,13 +8,31 @@
 import SwiftUI
 
 struct RecipeHighlightsView: View {
+    var highlights: [String]
+    
+    var allHighlights: String {
+        var newString = ""
+        
+        for index in 0..<highlights.count {
+            if index == highlights.count-1 {
+                newString += highlights[index]
+            }
+            else {
+                newString += highlights[index] + ", "
+            }
+        }
+        
+        return newString
+    }
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Text(allHighlights)
+            .font(Font.custom("Avenir", size: 15))
     }
 }
 
 struct RecipeHighlightsView_Previews: PreviewProvider {
     static var previews: some View {
-        RecipeHighlightsView()
+        RecipeHighlightsView(highlights: ["Vegetarian", "Healthy", "Easy-cleanup"])
     }
 }
